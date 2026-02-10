c0e540560c90cca9d432c0c70dfaef9f6d1cb9e9
GilbertoPaiva <gilberto.melo.paiva@gmail.com>
2026-02-10 12:09:18 -0300

    feat: implementar gerenciamento de filtros e edição de tarefas com Redux

--- src/store/reducers/tarefas.ts
+++ src/store/reducers/tarefas.ts
@@ -10,24 +10,26 @@ const initialState: TarefasState = {
   itens: [
     {
       id: 1,
-      titulo: 'Tarefa 1',
-      descricao: 'Descrição da tarefa 1',
+      titulo: 'Estudar JavaScript',
+      descricao:
+        'Estudar os conceitos básicos de React e criar um projeto simples',
       prioridade: enums.Prioridade.NORMAL,
       status: enums.Status.CONCLUIDA
     },
     {
       id: 2,
-      titulo: 'Tarefa 2',
-      descricao: 'Descrição da tarefa 2',
+      titulo: 'Estudar Redux',
+      descricao: 'Aprender a gerenciar estado com Redux e integrar com React',
       prioridade: enums.Prioridade.IMPORTANTE,
       status: enums.Status.PENDENTE
     },
     {
       id: 3,
-      titulo: 'Tarefa 3',
-      descricao: 'Descrição da tarefa 3',
+      titulo: 'Estudar TypeScript',
+      descricao:
+        'Aprender os conceitos básicos de TypeScript e como integrá-lo com React',
       prioridade: enums.Prioridade.URGENTE,
-      status: enums.Status.CONCLUIDA
+      status: enums.Status.PENDENTE
     }
   ]
 }
@@ -38,10 +40,18 @@ const tarefasSlice = createSlice({
   reducers: {
     remover: (state, action: PayloadAction<number>) => {
       state.itens = state.itens.filter((tarefa) => tarefa.id !== action.payload)
+    },
+    editar: (state, action: PayloadAction<Tarefa>) => {
+      const indexDaTarefa = state.itens.findIndex(
+        (t) => t.id === action.payload.id
+      )
+      if (indexDaTarefa >= 0) {
+        state.itens[indexDaTarefa] = action.payload
+      }
     }
   }
 })
 
-export const { remover } = tarefasSlice.actions
+export const { remover, editar } = tarefasSlice.actions
 
 export default tarefasSlice.reducer
